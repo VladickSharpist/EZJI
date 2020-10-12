c453f0ab6acae6c0fa5a983dcf167a453f86e587
Elihah_Wood <il.pavl.2002@mail.ru>
2020-10-12 20:38:15 +0300

    Add flag

--- Task4/Main.cs
+++ Task4/Main.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using flag;
 
 namespace Task4
 {
@@ -10,7 +11,8 @@ namespace Task4
     {
         static void Main(string[] args)
         {
-
+            Flag_Menu a=new Flag_Menu();
+            a.flag_menu();
         }
     }
 }
