35330de299ac959e0cb89d73af1de972bdf2b0ac
Mishinator <mishaneverovsky@gmail.com>
2020-10-12 03:48:19 +0300

    ням

--- task3/Main.cs
+++ task3/Main.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Utki;
 using Seif;
+using Counter;
 
 namespace task3
 {
@@ -20,6 +21,8 @@ namespace task3
             element1.Chek_id();
             element2.Chek_id();
             Console.ReadLine();
+            Cou.MyFun();
+           
         }
 
     }
