6bbf05cb51583b7c29006e00f2cc1199587d8202
Elihah_Wood <il.pavl.2002@mail.ru>
2020-10-19 18:26:37 +0300

    Add Task5
    Add Try/catch(2)

---
+++ Task5/Main.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TC_Fig;
+
+namespace Task5
+{
+    class Progarm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2 using\n3 Try/Catch\n ");
+            int cond = int.Parse(Console.ReadLine());
+
+            switch (cond)
+            {
+                case 2:
+                    {
+
+                    }break;
+                case 3:
+                    {
+                        TC a = new TC();
+                        a.Menu();
+
+                    }break;
+                    
+            }
+        }
+    }
+}
