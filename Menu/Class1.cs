1598d3e20a09d1678d43df2fd241ba2912ba8d6d
Vlad <vlad.eroshenko.96@mail.ru>
2020-10-23 19:30:10 +0300

    lib

---
+++ Menu/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu
+{
+    public class Class1
+    {
+        public delegate void Try();
+        public static void Menu()
+        {
+            Console.WriteLine("Enter task number");
+            var k = Console.ReadLine();
+            int.TryParse(k);
+        }
+    }
+}
