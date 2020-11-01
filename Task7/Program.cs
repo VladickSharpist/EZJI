75a88a2c68e2356d8c0ead9efe6ad930ef6e6e93
Vlad <vlad.eroshenko.96@mail.ru>
2020-11-01 23:42:31 +0300

    Raduga

---
+++ Task7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ne bei za menu\n1: raduga\n2:Borba s geuami(ne redi)\n3:Potoki(ne redi)");
+            var k = Convert.ToInt32(Console.ReadLine());
+            switch (k)
+            {
+                case 1:Try.Fun();
+                    break;
+            }
+        }
+    }
+}
