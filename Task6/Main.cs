bb369d05ea5fcf57327666c5fdfd41b9c198de16
Mishinator <mishaneverovsky@gmail.com>
2020-11-02 00:09:21 +0300

    Try

--- Task6/Main.cs
+++ Task6/Main.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Date;
+using IndividualWord;
 
 namespace Task6
 {
---
+++ Task6/Word.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndividualWord
+{
+    class Try
+    {
+       public static void Main(string[] args)
+        {
+            string Word;
+            int Counter = 0;
+            Console.WriteLine("Enter the word:");
+            Word = Console.ReadLine();
+            var Value = Word.Split(' ');
+            Dictionary<string, int> IndividualWord = new Dictionary<string, int>();
+            for (int i = 0; i < Value.Length; i++) 
+            {
+                if (IndividualWord.ContainsKey(Value[i]))
+                {
+                    int value = IndividualWord[Value[i]];
+                    IndividualWord[Value[i]] = value + 1;
+                }
+
+                else
+                {
+                    IndividualWord.Add(Value[i], 1);
+                }
+            }
+            Console.WriteLine("Individual words:");
+            foreach (KeyValuePair<string, int> kvp in IndividualWord)
+            {
+                if (kvp.Value == 1)
+                {
+                    Console.WriteLine(kvp.Key);
+                    Counter++;
+                }
+            }
+            Console.WriteLine("Number of individual words: " + Counter);
+            Console.ReadKey();
+        }
+    }
+}
