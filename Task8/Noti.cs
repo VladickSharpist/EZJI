25b4d30554a8d927051023314e9096a29b27c949
Vlad <vlad.eroshenko.96@mail.ru>
2020-11-12 21:23:23 +0300

    выа

---
+++ Task8/Noti.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task8
+{
+
+    public interface IAsyncDisposable
+    {
+        void Dispose(List<Nota> list)
+        {
+            foreach(var note in list)
+            {
+                note.Dispose();
+            }
+        }
+    }
+    public class Nota
+    {
+        int _Time;
+        string sound;
+        public Nota(int time, string s)
+        {
+            _Time = time;
+            sound = s;
+        }
+        public void Dispose()
+        {
+            Thread.Sleep(_Time);
+            Console.WriteLine(sound);
+        }
+    }
+}
---
+++ Task8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Nota> bass = new List<Nota>();
+            List<Nota> kick = new List<Nota>();
+            List<Nota> baraban = new List<Nota>();
+
+
+        }
+    }
+}
